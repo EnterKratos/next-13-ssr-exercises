551d8b02e48f1aa74df346fdd6cd45b60f5b2f67
EnterKratos <15807441+EnterKratos@users.noreply.github.com>
2023-09-27 16:07:39 +0100

    Suppress hydration mismatch warning
    This is safe in this example as we would only be losing the stale time value from the server.

--- src/app/exercises/01-clock/Clock.js
+++ src/app/exercises/01-clock/Clock.js
@@ -16,7 +16,7 @@ function Clock() {
   }, []);
 
   return (
-    <p className="clock">{format(time, 'hh:mm:ss.S a')}</p>
+    <p suppressHydrationWarning className="clock">{format(time, 'hh:mm:ss.S a')}</p>
   );
 }
 
